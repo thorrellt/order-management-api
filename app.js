2b6685f3a126cddd8f0239e123a7cdfc0ff646a6
ThorrellT <thorrellt@gmail.com>
2021-12-30 00:18:05 -0500

    add logic to prevent CORS errors

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express =  require('express');
+const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
@@ -7,16 +7,30 @@ const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow_Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods',
+        'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 // Routes to handle request
-app.use('/products' , productRoutes);
-app.use('/orders' , orderRoutes);
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('not found');
-    error.status= 404;
+    error.status = 404;
 
     next(error);
 });
