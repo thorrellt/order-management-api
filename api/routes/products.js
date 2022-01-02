2fe24ab1ddc081a62c4b3b032918a92996469e6a
ThorrellT <thorrellt@gmail.com>
2022-01-02 00:18:01 -0500

    add MongoDB & Mongoose. add product model and basic product POST logic

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+Product = require ('../models/product');
 
 router.get('/',(req, res, next) => {
     res.status(200).json({
@@ -7,14 +10,26 @@ router.get('/',(req, res, next) => {
     });
 });
 
-router.post('/',(req, res, next) => {
-    const product = {
-        name: req.body.name,
-        price: req.body.price
-    };
-    res.status(201).json({
-        message: 'Handling POST request to /products',
-        createdProduct: product
+router.post("/", (req, res, next) => {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price
+  });
+  product
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "Handling POST requests to /products",
+        createdProduct: result
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
     });
 });
 
--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect(
+    "mongodb+srv://node-shop:" + 
+      process.env.MONGO_ATLAS_PW + 
+      "@node-rest-shop.qkpv6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
