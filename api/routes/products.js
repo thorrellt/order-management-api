29149904a8db7663144c2fd0d212dc30ca30c724
ThorrellT <thorrellt@gmail.com>
2022-01-02 00:50:30 -0500

    add logic to GET products by id

--- api/routes/products.js
+++ api/routes/products.js
@@ -33,19 +33,25 @@ router.post("/", (req, res, next) => {
     });
 });
 
-router.get('/:productId', (req, res, next) => {
-    const id = req.params.productId;
-    if (id == 'special') {
-        res.status(200).json({
-            message: 'You discovered the special ID',
-            id: id
-        })
-    } else {
-        res.status(200).json({
-            message: 'You passed an ID'
-        });
-    }
-})
+router.get("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  Product.findById(id)
+    .exec()
+    .then(doc => {
+      console.log("From database", doc);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res
+          .status(404)
+          .json({ message: "No valid entry found for provided ID" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+});
 
 router.patch('/:productId', (req, res, next) => {
     res.status(200).json({
